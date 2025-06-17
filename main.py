9c3c9691bf06207e45f9c8e838f552f8e0852d78
Calvin Gutierrez <Calvinraphaelg+github2@gmail.com>
2025-06-18 00:22:09 +0700

    added sys import

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from stats import get_book_text
+from stats import get_book_text, sort_on
+import sys
+
+USER_INPUT = sys.argv
 
 
 def main():
-    num_words, character_count = get_book_text("books/frankenstein")
-    print(f'{num_words} words found in the document')
-    print(character_count)
-
+    if len(USER_INPUT) != 2:
+        print("Must include title of a book")
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    print(USER_INPUT)
+    num_words, character_count, character_list = get_book_text(USER_INPUT[1])
+    print(f'Found {num_words} total words')
+    character_list.sort(reverse=True, key=sort_on)
+    for char_data in character_list:
+        print(f'{char_data["char"]}: {char_data["num"]} ')
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -6,13 +6,20 @@ def get_stats(lst):
                 book_characters[char] = 1
             else:
                 book_characters[char] += 1
-    return book_characters
+    char_list = []
+    for char, count in book_characters.items():
+        char_list.append({"char": char, "num": count})
+
+    return book_characters, char_list
 
 
 def get_book_text(file_path: str) -> int:
     with open(file_path, "r") as file:
         file_content = file.read()
         list_words = file_content.split()
-        characters_dict = get_stats(list_words)
+        characters_dict, character_list = get_stats(list_words)
         word_count = len(list_words)
-    return word_count , characters_dict
+    return word_count, characters_dict, character_list
+
+def sort_on(dct):
+    return dct["num"]
